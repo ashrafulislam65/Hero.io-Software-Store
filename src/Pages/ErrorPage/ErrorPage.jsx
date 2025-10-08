ad3d5873deaeae7e42f8364364445c10655da410
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2025-10-09 02:46:49 +0600

    Error page done

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import Err from '../../assets/error-404.png';
+import Footer from '../../Components/Footer/Footer';
+import Navbar from '../../Components/Header/Navbar';
 
 const ErrorPage = () => {
     return (
-        <div>
-            404 not found
-        </div>
+        <>
+            <Navbar></Navbar>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content text-center">
+                    <div className="max-w-lg">
+                        <img src={Err} alt="" />
+                        <h1 className=" md:text-3xl lg:text-5xl font-bold">Oops, page not found!</h1>
+                        <p className="py-6">
+                            The page you are looking for is not available.
+                        </p>
+                        <button className="btn bg-linear-65 from-[#632ee3] to-[#9f62f2] text-[#ffffff]">Go Back!</button>
+                    </div>
+                </div>
+            </div>
+            <Footer></Footer>
+        </>
     );
 };
 
