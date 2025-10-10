4e91607d7db0bc9553b01d93cb1b3736d0f2c6e8
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2025-10-10 06:59:36 +0600

    Sorting by download count functionality added

--- src/Pages/InstalledList/InstalledList.jsx
+++ src/Pages/InstalledList/InstalledList.jsx
@@ -5,14 +5,26 @@ import { getStoredApps } from '../../Utility/AddToDB';
 import InstalledCard from '../../Components/InstalledCard/InstalledCard';
 const InstalledList = () => {
     const [installedList, setInstalledList] = useState([]);
+    const [sortBy, setSortBy] = useState("");
     const data = useLoaderData();
     console.log(data);
     useEffect(() => {
         const installedAppsData = getStoredApps();
-        const installedApps = installedAppsData.map(id=>parseInt(id));
+        const installedApps = installedAppsData.map(id => parseInt(id));
         const myInstalledApps = data.filter(app => installedApps.includes(app.id));
         setInstalledList(myInstalledApps);
     }, [])
+    const handleSort = (type) => {
+        setSortBy(type);
+        if (type === "High-Low") {
+            const sortedList = [...installedList].sort((a, b) => b.downloads - a.downloads);
+            setInstalledList(sortedList);
+        }
+        if (type === "Low-High") {
+            const sortedList = [...installedList].sort((a, b) => a.downloads - b.downloads);
+            setInstalledList(sortedList);
+        }
+    }
 
     return (
         <div>
@@ -29,12 +41,13 @@ const InstalledList = () => {
 
                 <div className=" flex justify-between  py-20">
                     <h3><span>{installedList.length}</span> Apps Found</h3>
-                    <select className="" id="sort" name="sort">
-                        <option value="bd">Sort By Size</option>
-                        <option value="in">India</option>
-                        <option value="us">United States</option>
-                        <option value="uk">United Kingdom</option>
-                    </select>
+                    <div className="dropdown dropdown-start">
+                        <div tabIndex={0} role="button" className="btn m-1">Sorted By Download: {sortBy?sortBy:""} ⬇️</div>
+                        <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                            <li><a onClick={()=>handleSort("High-Low")}>High-Low</a></li>
+                            <li><a onClick={()=>handleSort("Low-High")}>Low-High</a></li>
+                        </ul>
+                    </div>
 
 
 
